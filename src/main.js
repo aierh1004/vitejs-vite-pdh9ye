eac16cf5af2a1ed89c475fd1869455f35a1ceb58
aierh1004 <penny.whe@gmail.com>
2022-06-22 16:28:50 +0800

    2 files modified, 5 added, 1 deleted

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// import { createApp } from 'vue'
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import Vue from 'vue';
+new Vue({
+  el: '#app',
+  render: (h) => h(App),
+});
+
+// createApp(App).mount('#app')
